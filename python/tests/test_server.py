cf0e8fc94183f26e35b88eaac1d939fb512e151d
ruaridhg <ruaridhg@users.noreply.github.com>
2025-10-17 12:18:00 +0100

    Fix linting

--- python/tests/test_server.py
+++ python/tests/test_server.py
@@ -1,8 +1,11 @@
 import pytest
 
+
 @pytest.mark.usefixtures("remove_test_data")
 def test_xnatPlugin_installed(xnat_connection, plugin_version):
     assert "xnatPlugin" in xnat_connection.session.plugins
     xnat_plugin = xnat_connection.session.plugins["xnatPlugin"]
     assert xnat_plugin.version == f"{plugin_version}-xpl"
-    assert xnat_plugin.name == "XNAT plugin" #XNAT 1.8 ISMRMRD plugin from plugin java class
+    assert (
+        xnat_plugin.name == "XNAT plugin"
+    )  # XNAT 1.8 ISMRMRD plugin from plugin java class
